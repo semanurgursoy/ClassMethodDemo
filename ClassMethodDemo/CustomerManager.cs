b341b68b03bd0e90e1941f02f359173577fd2419
gursoy <gursoyysemanur@gmail.com>
2021-02-04 00:12:42 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager<T>
+    {
+
+        public List<Customer> customers = new List<Customer>();
+
+        public void add(Customer customer)
+        {
+            customers.Add(customer);
+        }
+        public void delete(Customer customer)
+        {
+            customers.Remove(customer);
+        }
+        public void deleteManuel(int id)
+        {
+            for(int i=0;i<customers.Count;i++)
+            {
+                if (id == customers[i].Id)
+                {
+                    customers.Remove(customers[i]);
+                }
+            }
+        }
+        public void list()
+        {
+            for(int i = 0; i < customers.Count; i++)
+            {
+                Console.WriteLine(customers[i].Id+"\t"+customers[i].FirstName+"\t"+customers[i].LastName);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 50;
+            customer1.FirstName = "John";
+            customer1.LastName = "Locke";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 51;
+            customer2.FirstName = "John Ronald Reuel";
+            customer2.LastName = "Tolkien";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 52;
+            customer3.FirstName = "Paul";
+            customer3.LastName = "McCartney";
+
+            CustomerManager<Customer> customerManager = new CustomerManager<Customer>();
+            customerManager.add(customer1);
+            customerManager.add(customer2);
+            customerManager.add(customer3);
+
+            int id; string name, lastName;
+            Console.WriteLine("Deger eklemek icin 1,devam etmek icin 2 tuslayiniz..");
+            int val = Convert.ToInt16(Console.ReadLine());
+            while (val == 1)
+            {
+                Console.WriteLine("Eklemek istediginiz musteriye ait Id,İsim,Soyisim giriniz..");
+                id = Convert.ToInt16(Console.ReadLine());
+                name = Convert.ToString(Console.ReadLine());
+                lastName = Convert.ToString(Console.ReadLine());
+                Customer customer = new Customer();
+                customer.Id = id;
+                customer.FirstName = name;
+                customer.LastName = lastName;
+                customerManager.add(customer);
+                Console.WriteLine("Deger eklemeye devam etmek icin 1,islemden cikis yapmak icin 2 tuslayiniz..");
+                val = Convert.ToInt16(Console.ReadLine());
+            }
+            customerManager.list();
+            Console.WriteLine("Deger silmek icin 1,devam etmek icin 2 tuslayiniz..");
+            val = Convert.ToInt16(Console.ReadLine());
+            while (val == 1)
+            {
+                Console.WriteLine("Silmek istediginiz musteriye ait Id giriniz..");
+                id = Convert.ToInt16(Console.ReadLine());
+                customerManager.deleteManuel(id);
+                Console.WriteLine("Deger silmeye devam etmek icin 1,islemden cikis yapmak icin 2 tuslayiniz..");
+                val = Convert.ToInt16(Console.ReadLine());
+            }
+
+            customerManager.list();
+            customerManager.delete(customer2);
+            customerManager.list();
+        }
+    }
+}
